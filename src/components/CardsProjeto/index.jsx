31fe19a20466bfc571b44908a6ce23beb86bc6e8
Gabriela Barbosa <gabrielasbarbosa4@gmail.com>
2023-09-20 23:46:59 -0300

    puxando nome do projeto para o card e definindo o layout da pagina com os cards

--- src/components/CardsProjeto/index.jsx
+++ src/components/CardsProjeto/index.jsx
@@ -1,36 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function CardsProjeto() {
-    const nomesProjetos = ["Projeto A", "Projeto B", "Projeto C", "Projeto D", "Projeto E"]; 
-    const totalCards = nomesProjetos.length; 
+    const [nomesProjetos, setNomesProjetos] = useState([]);
 
-    const cardsPorLinha = 5; 
-    const gruposDeCards = [];
-    for (let i = 0; i < totalCards; i += cardsPorLinha) {
-        const grupo = [];
-        for (let j = i; j < i + cardsPorLinha && j < totalCards; j++) {
-            grupo.push(
-                <div className="card" key={j} style={{ width: '12rem', marginTop: '25px' }}>
-                    <div className='project-square' style={{ width: '165px', margin: '12px 12px 1px 12px' }}></div>
-                    <div className="card-body">
-                        <h5 className="card-title">{nomesProjetos[j]}</h5>
-                        <div className="progress" style={{ marginTop: '10px' }}>
-                            <div className="progress-bar" role="progressbar" style={{ width: '30%' }} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-        gruposDeCards.push(
-            <div className="d-flex flex-row justify-content-around" key={i}>
-                {grupo}
-            </div>
-        );
-    }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/projeto/listar');
+                const nomes = response.data.map(projeto => projeto.nome);
+                setNomesProjetos(nomes);
+            } catch (error) {
+                console.error('Erro ao obter os dados dos projetos:', error);
+                setNomesProjetos([]);
+            }
+        };
+
+        fetchData();
+    }, []);   
 
     return (
         <div>
-            {gruposDeCards}
+            {nomesProjetos.length > 0 ? (
+                <div>
+                    {nomesProjetos.map((nome, index) => (
+                        <div className="card" key={index} style={{ width: '12rem', marginTop: '25px' }}>
+                            <div className='project-square' style={{ width: '165px', margin: '12px 12px 1px 12px' }}></div>
+                            <div className="card-body">
+                                <h5 className="card-title">{nome}</h5>
+                                <div className="progress" style={{ marginTop: '10px' }}>
+                                    <div className="progress-bar" role="progressbar" style={{ width: '30%' }} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <p style={{ marginTop: '25px', color: 'red', fontWeight: 'bold'}}>Nenhum projeto encontrado.</p>
+            )}
         </div>
     );
 }
