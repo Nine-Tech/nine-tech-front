b57fa6a9ea334aad993cd32139c242cfc042718c
Thiago Zani <thiago.zani@uol.com.br>
2023-10-20 15:49:56 -0300

    Finalizacao estilizacao Navbar

--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -22,13 +22,10 @@ const Navbar = (props) => {
     <>
       <nav className="navbar shadow bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <a className="navbar-brand" href={route}>
-              <img src={logo} alt="9tech-logo" width="90"></img>
-            </a>
+          <a className="navbar-brand" href={route}>
+            <img src={logo} alt="9tech-logo" width="90"></img>
           </a>
-          
-          <div className="d-flex">
+          <div className="d-flex ms-auto me-2">
             <span className="fs-5 fw-bold">{userTitle}</span>
           </div>
           <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
@@ -37,30 +34,34 @@ const Navbar = (props) => {
           <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <div className="offcanvas-header">
               <h5 className="offcanvas-title" id="offcanvasNavbarLabel"><a className="navbar-brand" href={route}>
-            <img src={logo} alt="9tech-logo" width="90"></img>
-          </a></h5>
+                <img src={logo} alt="9tech-logo" width="90"></img>
+              </a></h5>
               <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
+            <div className="d-flex mx-3 ">
+              <span className="fs-5 fw-bold">{userTitle}</span>
+            </div>
+            <div className="m-2 border-bottom border-primary border-2" />
             <div className="offcanvas-body">
               <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                 <li className="nav-item">
-                <Link
-                  className="nav-link active fs-5 fw-bold"
-                  aria-current="page"
-                  to={route}
-                >
-                  Home
-                </Link>
+                  <Link
+                    className="nav-link  fs-5 fw-bold"
+                    aria-current="page"
+                    to={route}
+                  >
+                    Home
+                  </Link>
                 </li>
                 <li className="nav-item">
-                <button className="nav-link d-flex align-items-center" onClick={logout}>
-                  <h5 className="fs-5 fw-bold">SAIR</h5>
-                  <i className="fa fa-right-from-bracket ms-2 fa-2x" />
-                </button>
+                  <button className="nav-link d-flex align-items-center" onClick={logout}>
+                    <h5 className="fs-5 fw-bold">SAIR</h5>
+                    <i className="fa fa-right-from-bracket ms-2 fa-2x" />
+                  </button>
                 </li>
-                
+
               </ul>
-              
+
             </div>
           </div>
         </div>
