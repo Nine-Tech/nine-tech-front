495e06515592ff63165340245db8f5a09f790a71
Thiago Zani <thiago.zani@uol.com.br>
2023-10-20 10:39:00 -0300

    Alteracao Navbar

--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -11,7 +11,7 @@ const Navbar = (props) => {
     ? "/engenheirochefe"
     : `/liderprojeto/${id}`;
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const logout = () => {
     removeToken();
@@ -20,8 +20,54 @@ const Navbar = (props) => {
 
   return (
     <>
-      <nav className="navbar shadow  navbar-expand-lg bg-body-tertiary ">
-        <div className="container-fluid">          
+      <nav className="navbar shadow bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <a className="navbar-brand" href={route}>
+              <img src={logo} alt="9tech-logo" width="90"></img>
+            </a>
+          </a>
+          
+          <div className="d-flex">
+            <span className="fs-5 fw-bold">{userTitle}</span>
+          </div>
+          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel"><a className="navbar-brand" href={route}>
+            <img src={logo} alt="9tech-logo" width="90"></img>
+          </a></h5>
+              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                <Link
+                  className="nav-link active fs-5 fw-bold"
+                  aria-current="page"
+                  to={route}
+                >
+                  Home
+                </Link>
+                </li>
+                <li className="nav-item">
+                <button className="nav-link d-flex align-items-center" onClick={logout}>
+                  <h5 className="fs-5 fw-bold">SAIR</h5>
+                  <i className="fa fa-right-from-bracket ms-2 fa-2x" />
+                </button>
+                </li>
+                
+              </ul>
+              
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      {/* <nav className="navbar shadow navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
           <button
             className="navbar-toggler"
             type="button"
@@ -47,23 +93,20 @@ const Navbar = (props) => {
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-              <button className="nav-link d-flex align-items-center" onClick={logout}>
-            <h5 className="fs-5 fw-bold">SAIR</h5>
-              <i className="fa fa-right-from-bracket ms-2 fa-2x" />
-            </button>
+              <li className="nav-item ">
+                <button className="nav-link d-flex align-items-center" onClick={logout}>
+                  <h5 className="fs-5 fw-bold">SAIR</h5>
+                  <i className="fa fa-right-from-bracket ms-2 fa-2x" />
+                </button>
               </li>
-              
-              
-              
             </ul>
           </div>
           <div className="d-flex align-items-center">
             <span className="font-size-20 ">{userTitle}</span>
-            
+
           </div>
         </div>
-      </nav>
+      </nav> */}
     </>
   );
 };
