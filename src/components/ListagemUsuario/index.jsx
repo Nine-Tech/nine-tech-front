23e1476dbb4873ce958ed7c32dc0c97c7d0eaffa
Jean Faria <111452998+jeejinf@users.noreply.github.com>
2023-11-14 11:08:56 -0300

    Criação do Modal para a listagem

--- src/components/ListagemUsuario/index.jsx
+++ src/components/ListagemUsuario/index.jsx
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from "react";
 import Toast from "@/components/Toast";
+import Modal from "@/components/Modal";
+import "./style.scss";
 
-const ListagemUsuarios = () => {
+const ListagemUsuarios = ({ onEditUser }) => {
   const [usuarios, setUsuarios] = useState([]);
-  const [toast, setToast] = useState(false)
+  const [toast, setToast] = useState(false);
+  const [modalData, setModalData] = useState({
+    showModal: false,
+    userIdToDelete: null,
+    userToDelete: null,
+  });
+  const [deleteStatus, setDeleteStatus] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Simulando uma requisição para obter a lista de usuários
         const response = await window.axios.get("auth/lideres");
         setUsuarios(response.data);
-        console.log(response.data);
       } catch (error) {
         console.error("Erro na requisição:", error);
       }
@@ -21,19 +27,90 @@ const ListagemUsuarios = () => {
   }, []);
 
   const handleEdit = (id) => {
-    // Lógica para edição de usuário (redirecionamento para a página de edição, por exemplo)
     console.log(`Editar usuário com ID ${id}`);
+    onEditUser && onEditUser(id);
   };
 
-  const handleDelete = (id) => {
-    // Lógica para exclusão de usuário
-    console.log(`Excluir usuário com ID ${id}`);
+  const handleDelete = (id, nome) => {
+    setModalData({
+      showModal: true,
+      userIdToDelete: id,
+      userToDelete: nome,
+    });
+    setDeleteStatus(null); // Resetar o status ao abrir o modal
   };
+
+  const cancelDelete = () => {
+    setModalData({
+      showModal: false,
+      userIdToDelete: null,
+      userToDelete: null,
+    });
+    setDeleteStatus(null); // Resetar o status ao cancelar
+  };
+
+  const confirmDelete = async (id) => {
+    try {
+      // Realizar a solicitação DELETE
+      const response = await window.axios.delete(`auth/${id}`);
+
+      console.log(`Excluído usuário com ID ${id}`);
+      console.log(response.data);
+
+      // Atualizar o status para sucesso
+      setDeleteStatus("success");
+
+      // Feche o modal após a exclusão ou faça outras ações necessárias
+      setModalData({
+        showModal: true,
+        userIdToDelete: null,
+        userToDelete: null,
+      });
+
+      // Atualizar a lista de usuários após a exclusão, se necessário
+      const updatedUsers = usuarios.filter((usuario) => usuario.id !== id);
+      setUsuarios(updatedUsers);
+    } catch (error) {
+      console.error("Erro ao excluir o usuário:", error);
+
+      setModalData({
+        showModal: true,
+        userIdToDelete: null,
+        userToDelete: null,
+      });
+
+      // Atualizar o status para falha
+      setDeleteStatus("fail");
+    }
+  };
+
+  const modalContent =
+    deleteStatus === "success" ? (
+      <p>Usuário excluído com sucesso!</p>
+    ) : deleteStatus === "fail" ? (
+      <p>Falha ao excluir o usuário. Tente novamente mais tarde.</p>
+    ) : (
+      <>
+        <p className="confirmation-message">
+          Deseja realmente excluir este usuário?
+        </p>
+        <div className="user-details">
+          <p>
+            <b>ID: </b>
+            {modalData.userIdToDelete}
+          </p>
+          <p>
+            <b>Nome: </b>
+            {modalData.userToDelete}
+          </p>
+        </div>
+      </>
+    );
 
   return (
     <div>
-    <Toast show={toast} toggle={setToast} />
-      <h2>Lideres de Projeto:</h2>
+      <Toast show={toast} toggle={setToast} />
+      <h2 className="mt-3">Lideres de Projeto:</h2>
       <table className="table">
         <thead>
           <tr>
@@ -45,24 +122,57 @@ const ListagemUsuarios = () => {
         <tbody>
           {usuarios.map((usuario) => (
             <tr key={usuario.id}>
-              <td>{usuario.id}</td>
-              <td>{usuario.nome}</td>
-              <td>
+              <td className="col-1">{usuario.id}</td>
+              <td className="col-8">{usuario.nome}</td>
+              <td className="col-3">
                 {/* Ícone de Editar */}
                 <i
-                  className="fa fa-edit text-primary cursor-pointer"
+                  className="fa fa-edit text-primary pointer me-4"
                   onClick={() => handleEdit(usuario.id)}
                 />
+
                 {/* Ícone de Excluir */}
                 <i
-                  className="fa fa-trash text-danger cursor-pointer ml-2"
-                  onClick={() => handleDelete(usuario.id)}
+                  className="fa fa-trash text-danger pointer"
+                  onClick={() => handleDelete(usuario.id, usuario.nome)}
                 />
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+      <Modal
+        showModal={modalData.showModal}
+        handler={cancelDelete}
+        header={() => (
+          <h5>
+            {deleteStatus === "success"
+              ? "Exclusão Bem-sucedida"
+              : deleteStatus === "fail"
+              ? "Exclusão Falhou"
+              : "Confirmação de Exclusão"}
+          </h5>
+        )}
+        footer={() => (
+          <>
+            {deleteStatus !== "success" && deleteStatus !== "fail" && (
+              <>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => confirmDelete(modalData.userIdToDelete)}
+                >
+                  Excluir
+                </button>
+                <button className="btn btn-secondary" onClick={cancelDelete}>
+                  Cancelar
+                </button>
+              </>
+            )}
+          </>
+        )}
+      >
+        {modalContent}
+      </Modal>
     </div>
   );
 };
