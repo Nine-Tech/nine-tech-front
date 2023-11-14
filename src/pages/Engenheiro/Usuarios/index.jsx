68ea3a4d6cabacea94d9e55cd78db144bfb83246
Jean Faria <111452998+jeejinf@users.noreply.github.com>
2023-11-14 11:09:18 -0300

    Melhoria das páginas para o Usuário, agora ao clicar em editar será enviado para a edição de usuário

--- src/pages/Engenheiro/Usuarios/index.jsx
+++ src/pages/Engenheiro/Usuarios/index.jsx
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import BodyHeader from "@/components/BodyHeader";
-import ListagemUsuario from "@/components/ListagemUsuario"
+import ListagemUsuario from "@/components/ListagemUsuario";
+import EditarUsuario from "@/components/EditarUsuario";
 import CadastroUsuario from "@/components/CadastroUsuario";
 
 const navigation = [
-  { link: "#listagem", title: "Listagem" },
+  { link: "#listar", title: "Listagem" },
   { link: "#cadastrar", title: "Cadastrar" },
+  { link: "#editar", title: "Editar" },
 ];
 
 const Usuarios = () => {
+  const [usuarioEditandoId, setUsuarioEditandoId] = useState(null);
+  
+  const handleEditarUsuario = (id) => {
+    // Define o ID do usuário que está sendo editado
+    setUsuarioEditandoId(id);
+
+    // Ativa a aba de edição
+    document.getElementById("Editar").click();
+  };
+
   return (
     <>
       <BodyHeader title={"Usuarios"} navigation={navigation} />
       <div className="my-3 tab-content">
 
         {/* Listagem de Usuários */}
-        <div className="tab-pane active" id="listagem" role="tabpanel">
-          <ListagemUsuario />
+        <div className="tab-pane active" id="listar" role="tabpanel">
+          <ListagemUsuario onEditUser={handleEditarUsuario} />
         </div>
 
         {/* Cadastro de Usuário */}
         <div className="tab-pane" id="cadastrar" role="tabpanel">
           <CadastroUsuario />
+        </div>
+
+        {/* Edição de Usuário */}
+        <div className="tab-pane" id="editar" role="tabpanel">
+          <EditarUsuario usuarioId={usuarioEditandoId} />
         </div>
       </div>
     </>
