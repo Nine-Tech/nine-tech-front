50aaeddfd999ca04edbb140d9f4678a6751cbb7e
Jean Faria <111452998+jeejinf@users.noreply.github.com>
2023-11-14 11:07:04 -0300

    Agora tem o Toast no Cadastro de Usuário

--- src/components/CadastroUsuario/index.jsx
+++ src/components/CadastroUsuario/index.jsx
@@ -11,6 +11,7 @@ function CadastroUsuario() {
   });
 
   const [toast, setToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -27,6 +28,7 @@ function CadastroUsuario() {
       await window.axios.post("auth/registro", usuario);
 
       // Se a request for bem-sucedida, exibe o toast de sucesso
+      setToastMessage("Usuário cadastrado com sucesso!");
       setToast(true);
 
       // Limpa os campos do formulário
@@ -38,53 +40,78 @@ function CadastroUsuario() {
       });
     } catch (error) {
       console.error("Erro na requisição:", error);
-      // Se houver um erro, você pode exibir um toast de erro ou lidar de outra forma
+
+      // Verifica se o erro é de validação (código 400)
+      if (error.response && error.response.status === 400) {
+        setToastMessage("E-mail já cadastrado!");
+      } else {
+        // Se for um erro diferente de validação, você pode lidar de outra forma
+        setToastMessage("Erro ao processar a requisição.");
+      }
+
+      setToast(true);
     }
   };
 
   return (
-    <div>
-      <Toast show={toast} toggle={() => setToast(false)} />
+    <div className="container mt-2">
+      <Toast show={toast} toggle={() => setToast(false)} type={toastMessage.includes("sucesso") ? "success" : "danger"}>
+        {toastMessage}
+      </Toast>
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="nome">Nome:</label>
-          <input
-            type="text"
-            id="nome"
-            name="nome"
-            value={usuario.nome}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="senha">Senha:</label>
-          <input
-            type="password"
-            id="senha"
-            name="senha"
-            value={usuario.senha}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="login">Login (E-mail):</label>
-          <input
-            type="email"
-            id="login"
-            name="login"
-            value={usuario.login}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        {/* Campo hidden de "role" */}
-        <input type="hidden" name="role" value="user" />
+      <div className="card p-4">
+        <h2 className="mb-3">Cadastro de Usuário</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-2">
+            <label htmlFor="nome" className="form-label">
+              Nome:
+            </label>
+            <input
+              type="text"
+              id="nome"
+              name="nome"
+              value={usuario.nome}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="senha" className="form-label">
+              Senha:
+            </label>
+            <input
+              type="password"
+              id="senha"
+              name="senha"
+              value={usuario.senha}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="login" className="form-label">
+              Login (E-mail):
+            </label>
+            <input
+              type="email"
+              id="login"
+              name="login"
+              value={usuario.login}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
+          {/* Campo hidden de "role" */}
+          <input type="hidden" name="role" value="user" />
 
-        <button type="submit">Cadastrar Usuario</button>
-      </form>
+          <button type="submit" className="btn btn-primary">
+            Cadastrar Usuário
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
