e2f83cb329bb51c4a1b0591460f1b31370b9e7d7
Gabriela Barbosa <gabrielasbarbosa3@gmail.com>
2023-11-24 00:54:47 -0300

    funcionalidade de adição de meses em caso de atraso incluida, faltando apenas a finalização do posicionamento do botão no bodyheader do engenheiro chefe

--- src/pages/Engenheiro/Projeto/index.jsx
+++ src/pages/Engenheiro/Projeto/index.jsx
@@ -1,18 +1,21 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import BodyHeader from "@/components/BodyHeader";
 import TabelaCronograma from "@/components/TabelaCronograma";
 import Dashboard from "../../../components/Dashboard/Dashboard";
 import LiderSelect from "../../../components/LiderSelect/LiderSelect";
 import { GraficoProjeto } from "../../../components/GraficoCurvaS/GraficoProjeto";
+import Modal from "@/components/Modal";
 
 const Projeto = () => {
-  const { id, itemId } = useParams();
+  const { id } = useParams();
 
   const [project, setProject] = useState({});
   const [packages, setPackages] = useState([]);
   const [cronograma, setCronograma] = useState({});
   const [idProjeto, setIdProjeto] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [novaDataFinal, setNovaDataFinal] = useState("");
 
   useEffect(() => {
     window.axios.get(`projeto/${id}`).then(({ data }) => {
@@ -28,6 +31,41 @@ const Projeto = () => {
       setCronograma(data);
     });
   }, [id]);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleConfirmar = async () => {
+    try {
+      if (!project.data_inicio) {
+        const response = await window.axios.get(`projeto/${id}`);
+        const projetoCompleto = response.data;
+        setProject(projetoCompleto);
+      }
+
+      const dadosAtualizados = {
+        nome: project.nome || '',
+        data_inicio: project.data_inicio || '',
+        data_final: novaDataFinal,
+      };
+
+      console.log('Dados Atualizados:', dadosAtualizados);
+
+      const response = await window.axios.put(`projeto/${id}`, dadosAtualizados);
+
+      setProject(response.data);
+      console.log('Projeto atualizado:', response.data);
+
+      handleCloseModal();
+    } catch (error) {
+      console.error('Erro ao atualizar o projeto:', error);
+    }
+  };
 
   const navigation = [
     { link: "#atribuicao", title: "Atribuição" },
@@ -45,7 +83,13 @@ const Projeto = () => {
         porcentagem={project.porcentagem}
         data_inicio={project.data_inicio}
         data_final={project.data_final}
-      />
+      >
+      <div className="mt-5">
+        <button className="btn btn-primary shadow" onClick={handleOpenModal}>
+          Alterar Data
+        </button>
+      </div>
+      </BodyHeader>
       <div className="my-5 tab-content">
         <div className="tab-pane active" id="atribuicao" role="tabpanel">
           <LiderSelect data={packages} />
@@ -54,12 +98,50 @@ const Projeto = () => {
           <Dashboard data={packages} />
         </div>
         <div className="tab-pane" id="cronograma" role="tabpanel">
-          <TabelaCronograma data={cronograma} idProjeto={idProjeto} dataInicio={project.data_inicio} dataFinal={project.data_final} />
+          <TabelaCronograma
+            data={cronograma}
+            idProjeto={idProjeto}
+            dataInicio={project.data_inicio}
+            dataFinal={project.data_final}
+          />
         </div>
         <div className="tab-pane" id="grafico" role="tabpanel">
           <GraficoProjeto />
         </div>
       </div>
+      <Modal size="lg" showModal={showModal} handler={handleCloseModal}>
+        <div className="modal-content">
+          <div className="modal-header">
+            <h4 className="modal-title">Alterar Data Final do Projeto</h4>
+          </div>
+          <div className="modal-body">
+            <label>
+              Insira a nova data final:
+              <br />
+              <input
+                type="date"
+                className="form-control"
+                value={novaDataFinal}
+                onChange={(e) => setNovaDataFinal(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="modal-footer">
+            <button
+              className="btn btn-secondary"
+              onClick={handleCloseModal}
+            >
+              Fechar
+            </button>
+            <button
+              className="btn btn-primary"
+              onClick={handleConfirmar}
+            >
+              Confirmar
+            </button>
+          </div>
+        </div>
+      </Modal>
     </>
   );
 };
